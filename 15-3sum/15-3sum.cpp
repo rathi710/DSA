89e39d2a3c976ca509e875f628e1801b113890cb
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-05-09 12:11:16 +0530

    Time: 175 ms (31.93%), Space: 20 MB (68.12%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -12,17 +12,27 @@ public:
             int l = i+1;
             int r = n-1;
             
-            // To avoid duplicates
+            // To avoid duplicates by incrementing i
             if (i > 0 && A[i] == A[i - 1])
                 continue;
             
+            // Keeping A[i] same, check for sum of triplets=0 
+            // from i+1 till the end of nums vector
             while(l<r)
             {
                 if(A[i]+A[l]+A[r] == 0){                //found it
                     ans.push_back( {A[i],A[l],A[r]} );
                     
-                    // Skipping duplicates
-                    while (l < r && A[l] == A[l + 1])  l++;
+                    // To skip duplicate elements at left pointer
+                    while(l<r && A[l]==A[l+1]) 
+                        l++;
+                    
+                    // To skip duplicate elements at left pointer
+                    while(l<r && A[r]==A[r-1]) 
+                        r--;
+                    // One unique triplet has been found.
+                    // Increment left & decrement right for  
+                    // next triplet to be unique
                     
                     l++; r--;
                 }
