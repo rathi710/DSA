16fa3588d3ba80b6252004c68a4ab94e8f19164e
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-05-12 10:41:59 +0530

    Time: 35 ms (34.71%), Space: 15 MB (81.33%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) 
+    {
+        if(!head) return NULL;
+        ListNode *tmp = head;
+        
+        // Omit the nodes from the start of list by moving head pointer
+        // if these nodes have to be deleted
+        // eg [7, 7, 7, 3] val = 7, then we have to delete all 7 and move head to 3
+        
+        while(head && head->val==val){
+            head=head->next;
+        }
+        
+        // Now our head is updated
+        // Now we will iterate our linked list using cur pointer
+        // we surely know that cur is not to be deleted because we came to it
+        // from the previous loop
+        
+        while(tmp and tmp->next){
+            if(tmp->next->val==val){
+                tmp->next = tmp->next->next;
+            }
+            else tmp=tmp->next;
+        }
+        return head;
+    }
+};
+
+
+//Approach: 1
+//* Solving the problem using two pointers: previous and current.
+
+//Approach: 2(implemented above)
+//*Solving the problem using a single pointer: current.
