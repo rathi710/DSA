a98dee0fa75c2580b81f8e43a95b765611aee6b9
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-05-13 23:09:52 +0530

    Time: 75 ms (16.87%), Space: 90.2 MB (9.02%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -29,3 +29,25 @@ public:
         
     }
 };
+
+
+
+//more optimised merge
+
+// int n1 = nums1.size(), n2 = nums2.size(), k = 0, l1 = 0, l2 = 0, n = n1 + n2;
+// 	int num[n / 2 + 1];
+// 	while (l1 < n1 && l2 < n2 && k <= n / 2)
+// 	{
+// 		if (nums1[l1] < nums2[l2])
+// 			num[k++] = nums1[l1++];
+// 		else
+// 			num[k++] = nums2[l2++];
+// 	}
+// 	while (l1 < n1 && k <= n / 2)
+// 		num[k++] = nums1[l1++];
+// 	while (l2 < n2 && k <= n / 2)
+// 		num[k++] = nums2[l2++];
+
+// 	if (n % 2 == 0)
+// 		return double(num[n / 2] + num[n / 2 - 1]) / 2;
+// 	return double(num[n / 2]);
