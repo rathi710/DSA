58a88a108ce3b32ef214ced28502382fb280250c
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-05-23 21:48:42 +0530

    Time: 85 ms (70.36%), Space: 38 MB (85.67%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -19,3 +19,60 @@ public:
         return false;
     }
 };
+
+//sol 1 : brute 
+// Time Complexity :- BigO(N^3)
+// Space Complexity :- BigO(1)
+
+// class Solution {
+// public:
+//     bool find132pattern(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 for(int k=j+1;k<n;k++)
+//                 {
+//                     if(nums[k]<nums[j] && nums[k]>nums[i])return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// 2nd solution : better
+
+// Time Complexity :- BigO(N^2)
+// Space Complexity :- BigO(1)
+
+// class Solution {
+// public:
+//     bool find132pattern(vector<int>& nums) {
+//         int n=nums.size();
+//         int m=nums[0];
+//         for(int j=0;j<n;j++)
+//         {
+//             for(int k=j+1;k<n;k++)
+//             {
+//                 if(nums[k]>m && nums[j]>nums[k])return true;
+//                 m=min(m,nums[j]);
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+// sol 3 : optimal
+
+// .firstly take a stack st.
+// .than we add a new number
+// firstly we pop out all the number that are small that number
+// in this way last elemnt pop than become s3
+// . we find maximum s3(recently poped number)
+
+// Time Complexity :- BigO(N)
+// Space Complexity :- BigO(N)
+// DONE ABOVE
