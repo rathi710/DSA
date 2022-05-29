431c7783e664f0536df824e3fbc19242f37a635f
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-05-29 18:04:06 +0530

    Time: 23 ms (23.26%), Space: 21.7 MB (33.67%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -16,7 +16,7 @@ public:
         if(!root) return true;
         if(root->val <= min || root->val >= max) return false;
         
-        //-1/+1 bcoz no two values can be same in a bst
+        //-1/+1 or = in above base case bcoz no two values can be same in a bst
         //every node is unique
         return helper(root->left,min,root->val) && helper(root->right,root->val,max);
     }
@@ -26,3 +26,30 @@ public:
         return helper(root, LONG_MIN, LONG_MAX);
     }
 };
+
+//above: Range based Approach
+
+//another approach: using inorder traversal
+
+//we will do inorder traversal which will be sorted if its a BST
+//maintain a prev pointer and check along the way that prev should be smaller than the current root
+
+//  void fun(Node*root,Node* &prev,int &f){
+         
+//         if(!root) return;
+//         fun(root->left,prev,f);
+//         if(prev && prev->data>=root->data){
+//             f = 0;
+//             return;
+//         }
+//         prev = root;
+//         fun(root->right,prev,f);
+//     }
+    
+//     bool isBST(Node* root) 
+//     {
+//        Node*prev = NULL;
+//        int f = 1;
+//        fun(root,prev,f);
+//        return f;
+//     }
