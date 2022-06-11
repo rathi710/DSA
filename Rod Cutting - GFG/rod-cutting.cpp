3dff35bbfb727fb8d2fe4d65833543bd240a0ea9
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-06-11 15:51:28 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+  public:
+  
+    //unbounded knapsack variation(literally same)  
+    int solve(int len[],int price[],int n)
+    {
+        int dp[n+1][n+1];
+        
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }    
+                else if(len[i-1]<=j){
+                    dp[i][j] = max(price[i-1] + dp[i][j-len[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][n];
+    }
+  
+    int cutRod(int price[], int n) 
+    {
+        int len[n];
+        for(int i=0;i<n;i++) len[i]=i+1;
+        
+        return solve(len, price, n);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
