927743eb80c4bae7523d77b2d41fa4b2bb0cecd3
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-06-22 14:11:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.28%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        int dp[n+1];
+        
+        //base case
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
+
+//fibonacci variation
