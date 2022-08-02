cad28faaefd263eec96383bc01e619193ead20de
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-08-02 23:20:22 +0530

    Time: 12 ms (53.36%), Space: 13.8 MB (26.08%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
+    //by dynamic programming
     int maxProduct(vector<int>& nums) 
     {
-        int n=nums.size();
-        int product=1;
-        int max_product=INT_MIN;
-        
-        for(int i=0;i<n;i++)
+       int r=nums[0];
+       int maxi=r;
+       int mini=r;
+       for(int i=1;i<nums.size();i++)
         {
-            product*=nums[i];
-            max_product=max(product,max_product);
-            if(product==0)
-                product=1;
+            if(nums[i]<0)
+            {
+                swap(maxi,mini);
+            }
+            maxi=max(nums[i],nums[i]*maxi);
+            mini=min(nums[i],nums[i]*mini);
+            r=max(r,maxi);
         }
-        
-        product=1;
-        for(int i=n-1;i>=0;i--)
-        {
-            product*=nums[i];
-            max_product=max(product,max_product);
-            if(product==0)
-                product=1;
-        }
-        
-        return max_product;
+        return r;
     }
 };
 
+
+
+//kadane algo
+// int maxProduct(vector<int>& nums) 
+//     {
+//         int n=nums.size();
+//         int product=1;
+//         int max_product=INT_MIN;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             product*=nums[i];
+//             max_product=max(product,max_product);
+//             if(product==0)
+//                 product=1;
+//         }
+        
+//         product=1;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             product*=nums[i];
+//             max_product=max(product,max_product);
+//             if(product==0)
+//                 product=1;
+//         }
+        
+//         return max_product;
+//     }
 
 
 //brute force(N^2)
