88cd58feae57434ccdd4c1ceae5ac99f6dc65f56
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-08-23 22:53:36 +0530

    Time: 15 ms (93.43%), Space: 13.7 MB (72.48%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    //using hashmap with priority queue
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> m;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+        vector<int> ans;
+        
+        for(auto it: nums){
+            m[it]++;
+        }
+        
+        for(auto it: m){
+            pq.push({it.second,it.first});
+            if(pq.size() > k) pq.pop();
+        }
+        while(!pq.empty()){
+            pair<int,int> t = pq.top();
+            pq.pop();
+            ans.push_back(t.second);
+        }
+        
+        return ans;
+    }
+};
+
+
+//without using priority queue:-
+
+    // bool static comp(pair<int,int> a, pair<int,int> b){
+    //     return a.second < b.second;
+    // }
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     map<int,int> mp;
+    //     vector<int> ans;
+    //     vector<pair<int,int>> v;
+    //     for(auto&it:nums){
+    //         mp[it]++;
+    //     }
+    //     for(auto it:mp) v.push_back(it);
+    //     sort(v.begin(),v.end(),comp);
+    //     for(int i=v.size()-1;i>=0;i--){
+    //         if(k == 0) break;
+    //         ans.push_back(v[i].first);
+    //         k--;
+    //     }
+    //     return ans;
+    // }
