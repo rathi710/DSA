b43bf702c7355e5eb3e018fc640b6d8af65d6f06
rathi710 <85077087+rathi710@users.noreply.github.com>
2022-08-28 23:49:46 +0530

    Time: 1 ms (42.74%), Space: 5.9 MB (52.88%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     //tabulation
     int tribonacci(int n) {
-        int dp[40];
+        int dp[n+1];
+        if(n==0) return 0;
+        if(n <=2) return 1;
         dp[0] = 0;
         dp[1] = dp[2] = 1;
         for(int i=3; i<=n; i++){
@@ -21,3 +23,10 @@ public:
         
 //         return tribonacci(n-1)+ tribonacci(n-2)+ tribonacci(n-3);
 //     }
+
+// Time Complexity : O(2^N).This is the most naive approach to calculate tribonacci number and recursion tree grows exponentially. There's a lot of repeated work that happens here.
+    
+
+
+//implemented above:-
+//Since the previous solution does a lot of repeated calculcations, we can optimize the approach by storing calculated results and returning them instead of recalculating them again and again.
