ec84359783d7864f1429e6e9a81420c8c1c21811
Vishesh Rathi <vr382003@gmail.com>
2022-09-06 22:52:22 +0530

    Time: 482 ms (5.17%), Space: 98.5 MB (74.52%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
+    //2-pointer approach
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
         int maxi=0;
         
-        for(int i=0; i<n1; i++)
-        {
-            int low = 0, high = n2-1;
-            
-            while(low <= high){
-                int mid = low + (high-low)/2;
-                
-                if(nums1[i] <= nums2[mid]){
-                    maxi = max(maxi,mid-i);
-                    low = mid+1;
-                }
-                else high = mid-1;
-                
+        int i=0,j=0;
+        while(i < n1 && j < n2){
+            if(nums1[i] <= nums2[j]){
+                maxi = max(maxi,j-i);
+                j++;
             }
+            else i++;
         }
         return maxi;
     }
 };
 
 
-//brute - force
+
+//using Binary-search :-
+//  int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         int maxi=0;
+        
+//         for(int i=0; i<n1; i++)
+//         {
+//             int low = 0, high = n2-1;
+            
+//             while(low <= high){
+//                 int mid = low + (high-low)/2;
+                
+//                 if(nums1[i] <= nums2[mid]){
+//                     maxi = max(maxi,mid-i);
+//                     low = mid+1;
+//                 }
+//                 else high = mid-1;
+                
+//             }
+//         }
+//         return maxi;
+//     }
+
+
+
+
+ //brute - force
  // int maxDistance(vector<int>& nums1, vector<int>& nums2) {
  //        int n1 = nums1.size();
  //        int n2 = nums2.size();
